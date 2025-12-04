e37cc8606535b7fc73a74d7e41ab6fd550b5bc22
Rohan Shah <shhrohan19@gmail.com>
2025-12-04 12:18:25 +0530

    Test: Introduce BaseIntegrationTest for context reuse

--- todo-app/src/test/java/com/example/todo/TaskCreateIntegrationTest.java
+++ todo-app/src/test/java/com/example/todo/TaskCreateIntegrationTest.java
@@ -2,29 +2,14 @@ package com.example.todo;
 
 import com.example.todo.model.Task;
 import com.example.todo.model.TaskStatus;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.transaction.annotation.Transactional;
 
 import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-@Transactional
-public class TaskCreateIntegrationTest {
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @Autowired
-    private ObjectMapper objectMapper;
+public class TaskCreateIntegrationTest extends BaseIntegrationTest {
 
     @Test
     void shouldCreateTask() throws Exception {
--- todo-app/src/test/java/com/example/todo/TaskMoveIntegrationTest.java
+++ todo-app/src/test/java/com/example/todo/TaskMoveIntegrationTest.java
@@ -7,22 +7,12 @@ import com.example.todo.repository.SwimLaneRepository;
 import com.example.todo.repository.TaskRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.transaction.annotation.Transactional;
 
 import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-@Transactional
-public class TaskMoveIntegrationTest {
-
-    @Autowired
-    private MockMvc mockMvc;
+public class TaskMoveIntegrationTest extends BaseIntegrationTest {
 
     @Autowired
     private TaskRepository taskRepository;
